bcf9110a3636221fb77e7fd3bde4a685602f1361
Steven Van Acker <steven@singularity.be>
2015-02-04 09:28:44 +0100

    support for pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='libformatstr',
+      version='1.0',
+      description='Create payloads for format string exploits',
+      author='hellman',
+      url='https://github.com/hellman/libformatstr',
+      packages=['libformatstr']
+      )
